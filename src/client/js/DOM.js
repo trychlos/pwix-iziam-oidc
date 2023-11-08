c2e040497a5837bc22f0272afa5f7113dad7e770
Pierre Wieser <pwieser@trychlos.org>
2023-11-08 22:39:19 +0100

    Define sameWidth() DOM function

--- src/client/js/DOM.js
+++ src/client/js/DOM.js
@@ -2,7 +2,31 @@
  * pwix:core-ui/src/client/js/DOM.js
  */
 
+import _ from 'lodash';
+
 CoreUI.DOM = {
+
+    // Set the same width on the given selectors
+    sameWidth( selectors ){
+        let promises = [];
+        selectors.every(( sel ) => {
+            promises.push( CoreUI.DOM.waitFor( sel+' td:first-child' ));
+            return true;
+        });
+        Promise.allSettled( promises )
+            .then(() => {
+                let widths = [];
+                selectors.every(( sel ) => {
+                    widths.push( document.querySelector( sel+' td:first-child' ).offsetWidth );
+                    return true;
+                });
+                const max = parseInt( 1+_.max( widths ));
+                selectors.every(( sel ) => {
+                    document.querySelector( sel+' td:first-child' ).style.width = max+'px';
+                    return true;
+                });
+            });
+    },
 
     // https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
     //  only initialize jQuery plugins when the DOM element is available
